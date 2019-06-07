73aed14465f2a1ac71f765042b92b6041e1a874b
akroune <33838895+akroune@users.noreply.github.com>
2019-06-07 19:17:33 +0200

    Add files via upload

---
+++ src/Arbre.h
@@ -0,0 +1,15 @@
+/*
+ * Arbre.h
+ *
+ *  Created on: 7 juin 2019
+ *      Author: Toshiba
+ */
+
+#ifndef ARBRE_H_
+#define ARBRE_H_
+
+
+
+
+
+#endif /* ARBRE_H_ */
---
+++ src/Noeud.h
@@ -0,0 +1,22 @@
+/*
+ * Noeud.h
+ *
+ *  Created on: 7 juin 2019
+ *      Author: Toshiba
+ */
+
+#ifndef NOEUD_H_
+#define NOEUD_H_
+
+
+
+class Noeud
+{
+public:
+	Noeud* gauche;
+	Noeud* droite;
+    int i ;
+};
+
+
+#endif /* NOEUD_H_ */
